b749718d8fa6054d17898074f6eefd0d68be87eb
scossar <scossar3@gmail.com>
2024-06-06 12:49:53 -0700

    add config file

--- Gemfile
+++ Gemfile
@@ -2,7 +2,10 @@
 
 source 'https://rubygems.org'
 
+gem 'dotenv'
 gem 'front_matter_parser'
-gem 'json', '~> 2.6', '>= 2.6.3'
+gem 'httparty', '~> 0.13.7'
 gem 'net-http', '~> 0.4.1'
 gem 'uri', '~> 0.10.0'
+gem 'yaml', '~> 0.1.0'
+gem 'yard'
--- publish_to_discourse.rb
+++ publish_to_discourse.rb
@@ -1,13 +1,58 @@
 # frozen_string_literal: true
 
-# require 'front_matter_parser'
-require 'json'
-require 'net-http'
-require 'uri'
+require 'dotenv'
+require 'front_matter_parser'
+require 'httparty'
+require 'yaml'
 
-def publish_to_discourse(_file_path)
-  # content = File.read(file_path)
-  # parsed = FrontMatterParser::Parser.parse_file(file_path)
-  # front_matter = parsed.front_matter
-  File.write('obsidian.txt', 'this is a test', mode: 'a')
+Dotenv.load
+
+class PublishToDiscourse
+  def initialize(filename)
+    @filename = filename
+    @api_key = ENV.fetch('API_KEY')
+    load_config
+  end
+
+  def load_config
+    config = YAML.load_file('config.yml')
+    @api_username = config['api_username']
+    @base_url = config['base_url']
+    @vault_path = config['vault_path']
+  end
+
+  def publish
+    content = File.read("#{@vault_path}#{@filename}")
+    _parsed, markdown, title, post_id = parse(content)
+    puts markdown
+
+    return unless title
+
+    response = publish_note(title:, post_id:, markdown:)
+    return unless response.message == 'OK'
+
+    JSON.parse(response.body)
+  end
+
+  def parse(content)
+    parsed = FrontMatterParser::Parser.new(:md).call(content)
+    front_matter = parsed.front_matter
+    markdown = parsed.content
+    title = front_matter['title']
+    post_id = front_matter['post_id']
+    [parsed, markdown, title, post_id]
+  end
+
+  def publish_note(title:, post_id:, markdown:)
+    headers = { 'Api-Key' => @api_key, 'Api-Username' => @api_username,
+                'Content-Type' => 'application/json' }
+    url = post_id ? "#{@base_url}/#{post_id}.json" : "#{@base_url}/posts.json"
+    body = JSON.generate({ title:, raw: markdown, category: 8,
+                           skip_validations: true })
+    method = post_id ? :put : :post
+    HTTParty.send(method, url, headers:, body:)
+  end
 end
+
+obj = PublishToDiscourse.new(ARGV[0])
+obj.publish
