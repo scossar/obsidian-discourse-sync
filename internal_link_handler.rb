ab5e9dcd852f4f3fb94f42fdc718b9cbeb59654c
scossar <scossar3@gmail.com>
2024-06-08 21:52:08 -0700

    create class for internal links; delete internal link module

---
+++ internal_link_handler.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class InternalLinkHandler
+  def initialize(markdown)
+    @markdown = markdown
+  end
+end
--- lib/internal_link_handler.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module HandleInternalLinks
-  def self.handle_internal_links(markdown); end
-end
